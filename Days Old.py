aca10814a532928244bfc089b42ddc55da85513f
razat249 <razat249@gmail.com>
2016-01-18 03:16:28 +0530

    basic search engine back-end

---
+++ Days Old.py
@@ -0,0 +1,71 @@
+# Credit goes to Websten from forums
+#
+# Use Dave's suggestions to finish your daysBetweenDates
+# procedure. It will need to take into account leap years
+# in addition to the correct number of days in each month.
+
+def isLeapYear(year):
+    if year % 4 != 0:
+        return 28
+    if year % 100 !=0:
+        return 29
+    if year % 400 != 0:
+        return 28
+    else:
+        return 29
+
+def daysInMonth(year,month):
+    if month == 1 or month == 3 or month == 5 or month == 7 or month == 8 or month == 10 or month == 12:
+        return 31
+    else:
+        if month == 2:
+            return isLeapYear(year)
+        else:
+            return 30
+
+def nextDay(year, month, day):
+    if day < daysInMonth(year,month):
+        return year, month, day + 1
+    else:
+        if month == 12:
+            return year + 1, 1, 1
+        else:
+            return year, month + 1, 1
+        
+def dateIsBefore(year1, month1, day1, year2, month2, day2):
+    if year1 < year2:
+        return True
+    if year1 == year2:
+        if month1 < month2:
+            return True
+        if month1 == month2:
+            return day1 < day2
+    return False        
+
+
+def daysBetweenDates(year1, month1, day1, year2, month2, day2):
+
+    assert not dateIsBefore(year2, month2, day2, year1, month1, day1)
+    days = 0
+    while dateIsBefore(year1, month1, day1, year2, month2, day2):
+        year1, month1, day1 = nextDay(year1, month1, day1)
+        days += 1
+    return days
+
+print daysBetweenDates(1995,4,23,2015,5,31)
+
+def test():
+    test_cases = [((2012,1,1,2012,2,28), 58), 
+                  ((2012,1,1,2012,3,1), 60),
+                  ((2011,6,30,2012,6,30), 366),
+                  ((2011,1,1,2012,8,8), 585 ),
+                  ((1900,1,1,1999,12,31), 36523)]
+    
+    for (args, answer) in test_cases:
+        result = daysBetweenDates(*args)
+        if result != answer:
+            print "Test with data:", args, "failed"
+        else:
+            print "Test case passed!"
+
+test()
